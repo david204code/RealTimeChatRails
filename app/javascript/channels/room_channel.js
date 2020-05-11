6baae8a2eb8c2466a08f610a60dc88d8fc52fcd6
David Wu <david204code@gmail.com>
2020-05-11 14:14:28 +0100

    Set up channels

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -2,6 +2,7 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("RoomChannel", {
   connected() {
+    console.log("we are live channel")
     // Called when the subscription is ready for use on the server
   },
 
